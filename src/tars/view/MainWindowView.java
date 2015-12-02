2d99a372b0d3121fa3d0c456599c545b3dd3e8de
Timur Arslanov <riasta@yandex.ru>
2015-12-02 15:07:48 +0300

    button added

--- src/tars/view/MainWindowView.java
+++ src/tars/view/MainWindowView.java
@@ -5,10 +5,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 //import com.mysql.jdbc.Statement;
 
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.SplitPane;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import tars.Main;
@@ -22,8 +24,22 @@ public class MainWindowView {
 	public static Scene scene;
 	public static Stage stage;
 	private static ScrollPane scrPane;
+	static FlowPane verGrid;
+	static FlowPane buttonPane;
+	static Button changeButton;
+	static Button addBeforeButton;
+	static Button addAfterButton;
+	static Button delButton;
+	
 	public MainWindowView(Model m) throws SQLException{
 		db = m;
+		changeButton = new Button("Изменить");
+		addBeforeButton = new Button("Добавить до");		
+		addAfterButton = new Button("Добавить после");
+		delButton = new Button("Удалить");
+		
+		verGrid = new FlowPane(Orientation.HORIZONTAL);
+		buttonPane = new FlowPane(Orientation.VERTICAL);
 		root = new SplitPane();
 		pane = new GridPane();
 		scrPane = new ScrollPane();
@@ -33,7 +49,8 @@ public class MainWindowView {
 		tree.setMaxSize(150, 500);
 		tree.setMinSize(150, 500);
 		tree.addEventHandler(MouseEvent.MOUSE_CLICKED, new MainController());
-		root.getItems().addAll(tree,scrPane);
+		verGrid.getChildren().addAll(scrPane,buttonPane);
+		root.getItems().addAll(tree,verGrid);
 		scene = new Scene(root);
 		stage = new Stage();
 		stage.setScene(scene);
